622a9c5950b9d921505165141d69b3a264ba2da1
Igor Poliakov <i-sonic@yandex.ru>
2022-11-29 17:14:31 +0300

    added admin

--- backend/foodgram/api/admin.py
+++ backend/foodgram/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Favorite, ShoppingCart, Ingridient, Tag, Recipe,
+                     RecipeIngridient, RecipeTag)
+
+
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
+admin.site.register(Ingridient)
+admin.site.register(Tag)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngridient)
+admin.site.register(RecipeTag)
