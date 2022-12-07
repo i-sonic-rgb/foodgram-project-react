8b4d28b7cefefaf4540b0381b6c7b22ea07cd6a6
Igor Poliakov <i-sonic@yandex.ru>
2022-12-07 21:36:28 +0300

    Updated view

---
+++ backend/foodgram/api/mixins.py
@@ -0,0 +1,16 @@
+from rest_framework import mixins, viewsets
+
+
+class ListRetrieveViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
+
+
+class ListViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
