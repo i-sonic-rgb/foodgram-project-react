f0a38853938ddb07a4846112a1975383d2e99cc2
Igor Poliakov <i-sonic@yandex.ru>
2022-12-07 22:26:02 +0300

    isort

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='password')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', default='*')
+ALLOWED_HOSTS = os.getenv('*')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
