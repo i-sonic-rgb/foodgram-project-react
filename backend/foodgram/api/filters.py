6e6f208ba572b453228862859a8a0779f0d15200
i-sonic-rgb <103962429+i-sonic-rgb@users.noreply.github.com>
2022-12-10 13:58:06 +0300

    Update filters.py

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -11,7 +11,7 @@ class RecipeFilter(django_filters.FilterSet):
     is_in_shopping_cart = django_filters.rest_framework.BooleanFilter(
         method='get_is_in_shopping_cart'
     )
-    # tags = django_filters.rest_framework.CharFilter(method='get_tags')
+    tags = django_filters.rest_framework.CharFilter(method='get_tags')
 
     class Meta:
         model = Recipe
@@ -22,11 +22,11 @@ class RecipeFilter(django_filters.FilterSet):
             'is_in_shopping_cart'
         ]
 
-    # def get_tags(self, queryset, name, value):
-    #     '''Filter recipe's tags by Tag instance names.'''
-    #     if value:
-    #         return queryset.filter(tags__slug=value)
-    #     return queryset
+    def get_tags(self, queryset, name, value):
+        '''Filter recipe's tags by Tag instance names.'''
+        if value:
+            return queryset.filter(tags__slug=value)
+        return queryset
 
     def get_is_favorited(self, queryset, name, value):
         '''Return recipes which added to favorite by user. Filter by bool.'''
